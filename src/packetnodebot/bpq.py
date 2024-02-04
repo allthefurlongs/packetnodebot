6ac24775ed6c9820ef651f2d26631433173720b5
Matt <neverpointless@gmail.com>
2024-02-04 22:01:24 +0000

    More fleshing out of the skeleton needed to pass data around where it needs to go

---
+++ src/packetnodebot/bpq.py
@@ -0,0 +1,65 @@
+import asyncio
+import yaml
+import discord
+import packetnodebot.discord
+
+
+class BpqInterface():
+    def __init__(self, bot_in_queue, bot_out_queue):
+        self.bot_in_queue = bot_in_queue
+        self.bot_out_queue = bot_out_queue
+
+    async def process_bot_incoming(self):
+        try:
+            while True:  ## TODO have some flag we set when we quit or whatever
+                message = await self.bot_in_queue.get()
+                print(f"BpqInterface received: {message}")
+
+                if message == 'hello':
+                    await self.bot_out_queue.put("Hey there")
+
+                self.bot_in_queue.task_done()
+        except Exception as e:
+            print(e)
+
+    # TODO handle disconnect, and we need a Queue to SEND commands over telnet too
+    async def telnet_passthru(self):
+        self.telnet_reader, self.telnet_writer = await asyncio.open_connection('127.0.0.1', 8010)
+        while True:
+            msg_bytes = await self.telnet_reader.readline()
+            msg_str = msg_bytes.decode("utf-8")
+            print(f"telnet received: {msg_str}")
+            await self.bot_out_queue.put(msg_str)
+
+
+async def main():
+    try:
+        with open('packetnodebot.yaml', 'r') as file:
+            conf = yaml.safe_load(file)
+
+        bot_in_queue = asyncio.Queue()
+        bot_out_queue = asyncio.Queue()
+
+        bpq = BpqInterface(bot_in_queue, bot_out_queue)
+
+        intents = discord.Intents.default()
+        intents.message_content = True
+        intents.members = True
+        discord_con = packetnodebot.discord.DiscordConnector(bot_in_queue=bot_in_queue, bot_out_queue=bot_out_queue, intents=intents)
+
+        discord_task = asyncio.create_task(discord_con.start(conf['discord']['token']))
+        bpq_process_bot_in_task = asyncio.create_task(bpq.process_bot_incoming())
+        discord_process_bot_out_task = asyncio.create_task(discord_con.process_bot_outgoing())
+
+        telnet_passthru_task = asyncio.create_task(bpq.telnet_passthru())
+
+        await asyncio.gather(discord_task, bpq_process_bot_in_task, discord_process_bot_out_task, telnet_passthru_task)
+
+    except Exception as e:
+        print(e)
+
+def bpqnodebot():
+    try:
+        asyncio.run(main())
+    except Exception as e:
+        print(e)
---
+++ src/packetnodebot/discord.py
@@ -0,0 +1,45 @@
+import discord
+
+
+class DiscordConnector(discord.Client):
+    def __init__(self, *args, bot_in_queue, bot_out_queue, **kwargs,):
+        super().__init__(*args, **kwargs)
+        self.bot_in_queue = bot_in_queue
+        self.bot_out_queue = bot_out_queue
+        self.sent_from = None
+    
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
+
+    async def on_message(self, message):
+        try:
+            # we do not want the bot to reply to itself
+            if message.author.id == self.user.id:
+                return
+
+            ## TODO eventually we'll need to pull out some other basics like user id or whatever in a generic way for abstraction and also auth, for now just send the message content itself
+            #await self.node_interface.received(message.content)
+            await self.bot_in_queue.put(message.content)
+
+            self.sent_from = message.author  ## TODO this just saves the author of the last message - obviously we'll need a way to manage who we're talking to better than this eventually! Probably bot commands with registration password or something and save something somewhere
+
+            # TODO when we have saved a user id that we want to interact with in future, we can re-get the "member" instance in order to send them messages using the user id which we will save, like this:
+            # member = self.get_user(self.sent_from.id)
+            # await member.send("hey again!")
+
+        except Exception as e:
+            print(e)
+    
+    async def process_bot_outgoing(self):
+        try:
+            while True:  ## TODO have some flag we set when we quit or whatever
+                message = await self.bot_out_queue.get()
+                if self.sent_from is not None:
+                    await self.sent_from.send(message)
+                    print(f"DiscordConnector sent: {message}")
+                else:
+                    print(f"Not sending message as no member populated yet")
+                self.bot_out_queue.task_done()
+        except Exception as e:
+            print(e)
